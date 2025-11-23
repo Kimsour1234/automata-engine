299fa9f80e2a69bc97cf490321ba52d57d652931
Kimsour1234 <contact@automatainspiring.com>
2025-11-23 10:31:56 +0100

    Update cold.py

--- api/cold.py
+++ api/cold.py
@@ -1,1 +1,22 @@
+# --------------------------------------------------
+# PYTHON — VERSION PROVISOIRE
+# Test lecture variable CENTRAL_DOGMA_ROOT_ID
+# --------------------------------------------------
 
+from http.server import BaseHTTPRequestHandler
+import json, os
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        central_id = os.environ.get("CENTRAL_DOGMA_ROOT_ID")
+
+        response = {
+            "status": "ok",
+            "central_dogma_root_id": central_id
+        }
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode("utf-8"))
